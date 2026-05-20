38eaf5b25732ba0515dbf02bd269fec594fb27e1
Arslan <arslanejaz61@gmail.com>
2026-05-20 17:09:25 +0500

    Remove Team section and pages from website and admin panel views

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -22,8 +22,6 @@ import PropertyForm from '../admin/PropertyForm'
 import ContactList from '../admin/ContactList'
 import AreaList from '../admin/AreaList'
 import AreaForm from '../admin/AreaForm'
-import TeamList from '../admin/TeamList'
-import TeamForm from '../admin/TeamForm'
 import Settings from '../admin/Settings'
 
 function ScrollToTop() {
@@ -62,9 +60,6 @@ export default function AppRouter() {
           <Route path="areas" element={<AreaList />} />
           <Route path="areas/new" element={<AreaForm />} />
           <Route path="areas/edit/:id" element={<AreaForm />} />
-          <Route path="team" element={<TeamList />} />
-          <Route path="team/new" element={<TeamForm />} />
-          <Route path="team/edit/:id" element={<TeamForm />} />
           <Route path="contacts" element={<ContactList />} />
           <Route path="settings" element={<Settings />} />
         </Route>
